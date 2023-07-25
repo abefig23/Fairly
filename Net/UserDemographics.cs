4e8562f5c9727ef2d42f21ec6a3d4644a8fffaaf
abefig23 <123043556+abefig23@users.noreply.github.com>
2023-07-24 17:18:49 -0700

    Add files via upload

---
+++ Net/UserDemographics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sabio.Models.Domain.UserDemographics
+{
+    public class UserDemographics
+    {
+        public string PreferredName { get; set; }
+
+        public string Pronunciation { get; set; }
+
+        public string AboutMe { get; set; }
+
+        public int GenderId { get; set; }
+
+        public int Id { get; set; }
+
+    }
+}
---
+++ Net/UserDemographicsAddRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.UserDemographicsRequests
+{
+    public class UserDemographicsAddRequest
+    {      
+        public string PreferredName { get; set; }
+        
+        public string Pronunciation { get; set; }
+
+        public string AboutMe { get; set; }
+        [Required]
+        public int GenderId { get; set; }
+
+    }
+}
